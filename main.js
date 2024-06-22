5b483eddde3e18d2b6fd42010cb6ea6492943e4b
Eliezer Nsengi <avaranstudios@gmail.com>
2024-06-22 20:24:25 +0200

    feat: taskbar v1 image not working

--- main.js
+++ main.js
@@ -1,37 +1,32 @@
 
+function main() {
 
-let newX = 0, newY = 0, startX = 0, startY = 0;
+    let newX = 0, newY = 0, startX = 0, startY = 0;
 
-const card = document.getElementById('card')
-const titleBar = document.querySelector('.title-bar')
+    const card = document.getElementById('card')
+    const titleBar = document.querySelector('.title-bar')
+    card.style.top = Math.floor(Math.random() * 20) + 'vh'
+    card.style.left = Math.floor(Math.random() * 70) + 'vw'
+    titleBar.addEventListener('mousedown', mouseDown)
 
-card.style.top = Math.floor(Math.random() * 20) + '%'
-card.style.left = Math.floor(Math.random() * 70) + '%'
+    function mouseDown(e) {
+        startX = e.clientX
+        startY = e.clientY
+        document.addEventListener('mousemove', mouseMove)
+        document.addEventListener('mouseup', mouseUp)
+    }
 
+    function mouseMove(e) {
+        newX = startX - e.clientX
+        newY = startY - e.clientY
+        startX = e.clientX
+        startY = e.clientY
+        card.style.top = (card.offsetTop - newY) + 'px'
+        card.style.left = (card.offsetLeft - newX) + 'px'
+    }
 
-titleBar.addEventListener('mousedown', mouseDown)
+    function mouseUp(e) {
+        document.removeEventListener('mousemove', mouseMove)
+    }
 
-function mouseDown(e) {
-    startX = e.clientX
-    startY = e.clientY
-
-    document.addEventListener('mousemove', mouseMove)
-    document.addEventListener('mouseup', mouseUp)
-}
-
-function mouseMove(e) {
-
-    newX = startX - e.clientX
-    newY = startY - e.clientY
-
-    startX = e.clientX
-    startY = e.clientY
-
-    card.style.top = (card.offsetTop - newY) + 'px'
-    card.style.left = (card.offsetLeft - newX) + 'px'
-}
-
-function mouseUp(e) {
-
-    document.removeEventListener('mousemove', mouseMove)
 }
