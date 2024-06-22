168efaf1465b617aaa0b66464eba70de7b5434ce
Eliezer Nsengi <avaranstudios@gmail.com>
2024-06-22 23:10:03 +0200

    feat: toggle visibility startmenu

--- src/scripts/taskbar.js
+++ src/scripts/taskbar.js
@@ -3,4 +3,13 @@ function taskbar() {
     setInterval(() => {
         document.querySelector('.taskbar__right').innerHTML = `${new Date().toLocaleTimeString()}`
     }, 1000)
+
+    document.querySelector('.taskbar__start').addEventListener('click', () => {
+        const startMenu = document.querySelector('.start_menu');
+        if (startMenu.style.display === 'block') {
+            startMenu.style.display = 'none';
+        } else {
+            startMenu.style.display = 'block';
+        }
+    })
 }
